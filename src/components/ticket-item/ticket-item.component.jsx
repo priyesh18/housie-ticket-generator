d2651d61eb3b6c81d16e9c8dcb11ff30dd80adbd
Priyesh <safal.priyesh@gmail.com>
2020-07-02 13:14:57 -0400

    fix imageurl try 2

--- src/components/ticket-item/ticket-item.component.jsx
+++ src/components/ticket-item/ticket-item.component.jsx
@@ -5,7 +5,7 @@ const TicketItem = ({ imageUrl, name}) => (
     <div className="item-container">
         <div className="box"></div>
         <div className="item-image">
-            <img src={'housie-ticket-generator/'+`${imageUrl}`} alt=""/>
+            <img src={`${imageUrl}`} alt=""/>
         </div>
         <div className="item-name">{ name }</div>
     </div>
