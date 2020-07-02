4d367cb22a426daabc79459ad994f0a0431d9b10
Priyesh <safal.priyesh@gmail.com>
2020-07-02 12:05:37 -0400

    Updates

---
+++ precache-manifest.d25d279042ff02bec13407fb371c7632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff395e63259e08ef27bddf3e08c083e7",
+    "url": "/github.io/housie-ticket-generator/index.html"
+  },
+  {
+    "revision": "0fa77cd0351968cbf959",
+    "url": "/github.io/housie-ticket-generator/static/css/main.e0e36cf4.chunk.css"
+  },
+  {
+    "revision": "5fde6be6f6d6ef711950",
+    "url": "/github.io/housie-ticket-generator/static/js/2.625100c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/housie-ticket-generator/static/js/2.625100c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa77cd0351968cbf959",
+    "url": "/github.io/housie-ticket-generator/static/js/main.8613d4f5.chunk.js"
+  },
+  {
+    "revision": "da0c4f963b3e9932f23d",
+    "url": "/github.io/housie-ticket-generator/static/js/runtime-main.6b2b31b7.js"
+  }
+]);
